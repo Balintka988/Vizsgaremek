3f4dffd7d0d64f7ef56e7fdcad2d7f32281fddc1
Szikora Balint <balint.szikora00@gmail.com>
2026-01-21 13:56:32 +0100

    update SQL

--- autoszerviz.sql
+++ autoszerviz.sql
@@ -15,6 +15,21 @@ CREATE TABLE cars (
     status VARCHAR(50) DEFAULT 'Nincs státusz',
     FOREIGN KEY (owner_id) REFERENCES users(id) ON DELETE CASCADE
 );
+CREATE TABLE services (
+    id INT AUTO_INCREMENT PRIMARY KEY,
+    name VARCHAR(100) NOT NULL,
+    price DECIMAL(10,2) NOT NULL,
+    work_hours DECIMAL(5,2) NOT NULL
+);
+
+INSERT INTO services (name, price, work_hours) VALUES
+('Olajcsere', 25000.00, 1.00),
+('Műszaki vizsgáztatás', 15000.00, 1.50),
+('Gumicsere', 9000.00, 0.75),
+('Féktárcsák cseréje', 25000.00, 2.00),
+('Futómű javítás', 45000.00, 3.50),
+('Szélvédőcsere', 35000.00, 2.50);
+
 
 CREATE TABLE bookings (
     id INT AUTO_INCREMENT PRIMARY KEY,
@@ -22,9 +37,15 @@ CREATE TABLE bookings (
     car_id INT NOT NULL,
     date DATETIME NOT NULL,
     note TEXT,
+    service_id INT,
+    hours DECIMAL(5,2),
+    cost DECIMAL(10,2),
+    description TEXT,
+    note_to_client TEXT,
     status VARCHAR(50) DEFAULT 'Várakozik',
     FOREIGN KEY (user_id) REFERENCES users(id) ON DELETE CASCADE,
-    FOREIGN KEY (car_id) REFERENCES cars(id) ON DELETE CASCADE
+    FOREIGN KEY (car_id) REFERENCES cars(id) ON DELETE CASCADE,
+    FOREIGN KEY (service_id) REFERENCES services(id)
 );
 
 CREATE TABLE notifications (
